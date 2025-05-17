b23d98c17626cfb2b52f1a49cb2d2f94acb89edc
Frontier-Lord200 <sirfrontier3@gmail.com>
2025-05-17 15:53:32 +0200

    Create delta.js

---
+++ delta.js
@@ -0,0 +1,20 @@
+var commands = [];
+
+function delta(info, func) {
+    var data = info;
+    data.function = func;
+    if (!data.dontAddCommandList) data.dontAddCommandList = false;
+    if (!info.desc) info.desc = '';
+    if (!data.fromMe) data.fromMe = false;
+    if (!info.category) data.category = 'misc';
+    if(!info.filename) data.filename = "Not Provided";
+    commands.push(data);
+    return data;
+}
+module.exports = {
+    delta,
+    AddCommand:delta,
+    Function:delta,
+    Module:delta,
+    commands,
+};
